73a78a8e0cd88fe4f58d8a9b754a04f11e970bfa
mrverdant13 <karlo.verde.1996@gmail.com>
2021-05-03 12:33:41 -0500

    fix: run manual initialization within the app zone

--- lib/core/app_starter.dart
+++ lib/core/app_starter.dart
@@ -8,30 +8,32 @@ import 'dependency_injection.dart';
 import 'flavors.dart';
 
 Future<void> startApp(Flavor flavor) async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  getIt.registerSingleton(flavor);
-
-  FlutterError.onError = (details) {
-    log(
-      details.exceptionAsString(),
-      stackTrace: details.stack,
-    );
-  };
-
   runZonedGuarded(
-    () => runApp(
-      flavor == Flavor.prod
-          ? const MyApp()
-          : Directionality(
-              textDirection: TextDirection.ltr,
-              child: Banner(
-                message: flavor.tag,
-                location: BannerLocation.topStart,
-                child: const MyApp(),
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      getIt.registerSingleton(flavor);
+
+      FlutterError.onError = (details) {
+        log(
+          details.exceptionAsString(),
+          stackTrace: details.stack,
+        );
+      };
+
+      runApp(
+        flavor == Flavor.prod
+            ? const MyApp()
+            : Directionality(
+                textDirection: TextDirection.ltr,
+                child: Banner(
+                  message: flavor.tag,
+                  location: BannerLocation.topStart,
+                  child: const MyApp(),
+                ),
               ),
-            ),
-    ),
+      );
+    },
     (error, stackTrace) => log(
       error.toString(),
       stackTrace: stackTrace,

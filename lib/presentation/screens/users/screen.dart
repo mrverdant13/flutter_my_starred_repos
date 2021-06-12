ce4e24a5e29e8a0eeed2365622e0b65655723963
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-12 04:20:50 -0500

    feat: create users screen

---
+++ lib/presentation/screens/users/screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../../../l10n/l10n.dart';
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(l10n.usersScreenTitle),
+      ),
+    );
+  }
+}

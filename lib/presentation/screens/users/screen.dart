173886f3fac45bb8ef1f5540afe98d7f981df9f9
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-12 04:41:10 -0500

    feat: implement users list load

--- lib/presentation/screens/users/screen.dart
+++ lib/presentation/screens/users/screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../application/cubit/users/users_cubit.dart';
 import '../../../l10n/l10n.dart';
 import '../../widgets/drawer.dart';
+import 'widgets/user_tile.dart';
+import 'widgets/wrapper.dart';
 
 class UsersScreen extends StatelessWidget {
   const UsersScreen();
@@ -9,11 +13,35 @@ class UsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(l10n.usersScreenTitle),
+    return Wrapper(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: Text(l10n.usersScreenTitle),
+        ),
+        drawer: const AppDrawer(),
+        body: BlocBuilder<UsersCubit, UsersState>(
+          builder: (context, usersState) => usersState.maybeWhen(
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            loaded: (users) => users.isEmpty()
+                ? Center(
+                    child: Text(l10n.noUsersMessage),
+                  )
+                : ListView(
+                    children: [
+                      ...users.iter.map(
+                        (user) => UserTile(
+                          key: ValueKey(user.id),
+                          user: user,
+                        ),
+                      ),
+                    ],
+                  ),
+            orElse: () => const SizedBox.shrink(),
+          ),
+        ),
       ),
-      drawer: const AppDrawer(),
     );
   }
 }

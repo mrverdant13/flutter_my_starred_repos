ca750a5a5f4a2a7b3419310b77d5c518c578c30f
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-12 04:21:11 -0500

    feat: register users screen

--- lib/presentation/routers/app_router.dart
+++ lib/presentation/routers/app_router.dart
@@ -2,12 +2,16 @@ import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 
 import '../screens/counter/screen.dart';
+import '../screens/users/screen.dart';
 
 @CustomAutoRouter(
   routes: [
     CustomRoute(
       page: CounterScreen,
       initial: true,
+    ),
+    CustomRoute(
+      page: UsersScreen,
     ),
   ],
 )
--- lib/presentation/routers/app_router.gr.dart
+++ lib/presentation/routers/app_router.gr.dart
@@ -8,6 +8,7 @@ import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
 import '../screens/counter/screen.dart' as _i3;
+import '../screens/users/screen.dart' as _i4;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
@@ -21,16 +22,31 @@ class AppRouter extends _i1.RootStackRouter {
           return _i3.CounterScreen();
         },
         opaque: true,
+        barrierDismissible: false),
+    UsersScreenRoute.name: (routeData) => _i1.CustomPage<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return _i4.UsersScreen();
+        },
+        opaque: true,
         barrierDismissible: false)
   };
 
   @override
-  List<_i1.RouteConfig> get routes =>
-      [_i1.RouteConfig(CounterScreenRoute.name, path: '/')];
+  List<_i1.RouteConfig> get routes => [
+        _i1.RouteConfig(CounterScreenRoute.name, path: '/'),
+        _i1.RouteConfig(UsersScreenRoute.name, path: '/users-screen')
+      ];
 }
 
 class CounterScreenRoute extends _i1.PageRouteInfo {
   const CounterScreenRoute() : super(name, path: '/');
 
   static const String name = 'CounterScreenRoute';
+}
+
+class UsersScreenRoute extends _i1.PageRouteInfo {
+  const UsersScreenRoute() : super(name, path: '/users-screen');
+
+  static const String name = 'UsersScreenRoute';
 }

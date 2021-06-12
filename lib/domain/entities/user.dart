e4b8ffb8a7cb4a60793c966ea697a5520a75ae2d
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-12 01:03:46 -0500

    feat: create user entity

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required int id,
+    required String name,
+    required String username,
+  }) = _User;
+}

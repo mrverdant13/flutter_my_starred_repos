b949e953e3211bc7a94bc8c0efd187d81c8d1530
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-14 01:42:55 -0500

    feat: imlement users remote data source with GQL client

---
+++ lib/infrastructure/data_sources/users/remote/gql_implementation.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:graphql/client.dart';
+
+import '../../../models/user.dart';
+import 'interface.dart';
+
+class UsersRDSImp extends UsersRDS {
+  const UsersRDSImp({
+    required this.gqlClient,
+  });
+
+  final GraphQLClient gqlClient;
+
+  @override
+  Future<Set<UserR>> getUsers() async {
+    // API endpoint doc:
+    // https://graphqlzero.almansi.me/
+
+    final queryResult = await gqlClient.query(
+      QueryOptions(
+        document: gql(
+          '''
+query getUsers {
+  users {
+    data {
+      id
+      name
+      username
+    }
+  }
+}
+''',
+        ),
+      ),
+    );
+
+    if (queryResult.exception?.linkException is NetworkException) {
+      throw const GetUsersException.offline();
+    }
+
+    final jsonUsers = queryResult.data!['users']['data'] as List;
+
+    final users = jsonUsers.map(
+      (jsonUser) {
+        jsonUser['id'] = int.tryParse(jsonUser['id'] as String);
+        return UserR.fromJson(
+          jsonUser as Map<String, dynamic>,
+        );
+      },
+    );
+
+    return users.toSet();
+  }
+}

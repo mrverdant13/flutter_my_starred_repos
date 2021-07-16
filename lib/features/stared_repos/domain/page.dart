c28ecf41d90428206658af345408ffffab410c97
Karlo Verde <karlo.verde.1996@gmail.com>
2021-07-15 23:49:22 -0500

    feat: create page entity

---
+++ lib/features/stared_repos/domain/page.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'page.freezed.dart';
+
+@freezed
+class Page<T> with _$Page<T> {
+  const factory Page({
+    required int lastPage,
+    required List<T> elements,
+  }) = _Page;
+}

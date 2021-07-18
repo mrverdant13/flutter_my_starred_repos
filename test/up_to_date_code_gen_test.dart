f8934fb3d35efcd84267f782b39dd240eb55cae9
Karlo Verde <karlo.verde.1996@gmail.com>
2021-07-17 23:37:35 -0500

    ci: ensure up-to-date code generation
    Check state on CI only

---
+++ test/up_to_date_code_gen_test.dart
@@ -0,0 +1,8 @@
+@Tags(['ci-only'])
+
+import 'package:build_verify/build_verify.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Ensure up-to-date code generation', expectBuildClean);
+}

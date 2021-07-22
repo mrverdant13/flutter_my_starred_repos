90f9d7b96262d59c691106a5c33b0165f5998516
Karlo Verde <karlo.verde.1996@gmail.com>
2021-07-22 00:33:00 -0500

    feat: identify unmodified data

--- lib/features/stared_repos/infrastructure/data_sources/stared_repos_rds/interface.dart
+++ lib/features/stared_repos/infrastructure/data_sources/stared_repos_rds/interface.dart
@@ -10,10 +10,14 @@ abstract class StaredReposRDS {
   /// Creates a stared repositories remote data source.
   const StaredReposRDS(); // coverage:ignore-line
 
-  /// Retrieves a page of GitHub stared repositories.
+  /// Retrieves a [Page] of starred [GithubRepoDto]s by the [page] they belong
+  /// to.
   ///
   /// Throws a [GetStaredReposPageException.offline] exception when there is no
   /// Internet connection.
+  ///
+  /// Throws a [GetStaredReposPageException.unmodified] exception when there is no
+  /// changes in data compared to the previously fetched version.
   Future<Page<GithubRepoDto>> getStaredReposPage({
     required int page,
   });
@@ -23,7 +27,16 @@ abstract class StaredReposRDS {
 /// stared repositories.
 @freezed
 class GetStaredReposPageException with _$GetStaredReposPageException {
+  const GetStaredReposPageException._();
+
   /// An exception thrown when there is no Internet connection.
   const factory GetStaredReposPageException.offline() =
       _GetStaredReposPageExceptionOffline;
+
+  /// An exception thrown when the data has not been modified compared to the
+  /// previously fetched one.
+  const factory GetStaredReposPageException.unmodified() =
+      _GetStaredReposPageExceptionUnmodified;
+
+  // bool get isOffline => this is _GetStaredReposPageExceptionOffline
 }

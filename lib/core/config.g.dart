b0f1072920dcf4746bf9fe23b1b96e447bbf6d25
Karlo Verde <karlo.verde.1996@gmail.com>
2021-08-15 19:45:04 -0500

    fix: drop legacy gen src

--- lib/core/config.g.dart
+++ lib/core/config.g.dart
@@ -27,24 +27,3 @@ Map<String, dynamic> _$_$_AppConfigToJson(_$_AppConfig instance) =>
     <String, dynamic>{
       'githubAuthConfig': instance.githubAuthConfig,
     };
-
-_$_GithubAuthConfig _$_$_GithubAuthConfigFromJson(Map json) {
-  return $checkedNew(r'_$_GithubAuthConfig', json, () {
-    $checkKeys(json,
-        allowedKeys: const ['clientId', 'clientSecret'],
-        requiredKeys: const ['clientId', 'clientSecret'],
-        disallowNullValues: const ['clientId', 'clientSecret']);
-    final val = _$_GithubAuthConfig(
-      clientId: $checkedConvert(json, 'clientId', (v) => v as String),
-      clientSecret: $checkedConvert(json, 'clientSecret', (v) => v as String),
-    );
-    return val;
-  });
-}
-
-Map<String, dynamic> _$_$_GithubAuthConfigToJson(
-        _$_GithubAuthConfig instance) =>
-    <String, dynamic>{
-      'clientId': instance.clientId,
-      'clientSecret': instance.clientSecret,
-    };

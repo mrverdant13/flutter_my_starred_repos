dd1c81edbdd3cb9c51492d49837801676d961d05
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-26 20:51:08 -0500

    ci: use remote `coverde` tool

--- cli/bin/cov.dart
+++
@@ -1,5 +0,0 @@
-import 'package:utils/commands/cov/command.dart' as cov;
-
-Future<void> main(List<String> arguments) async {
-  await cov.run(arguments);
-}
--- cli/bin/rm.dart
+++
@@ -1,5 +0,0 @@
-import 'package:utils/commands/rm/command.dart' as rm;
-
-Future<void> main(List<String> arguments) async {
-  await rm.run(arguments);
-}

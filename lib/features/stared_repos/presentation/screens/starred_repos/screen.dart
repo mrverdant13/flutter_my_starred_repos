0ebe0ea6a49e13d48ba813676f41ef78086160f2
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-23 00:47:03 -0500

    refactor: use `riverpod` only

--- lib/features/stared_repos/presentation/screens/starred_repos/screen.dart
+++ lib/features/stared_repos/presentation/screens/starred_repos/screen.dart
@@ -5,123 +5,111 @@ import 'package:flutter_my_starred_repos/features/stared_repos/core/dependency_i
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:starred_repos/starred_repos.dart';
 
-part 'wrapper.dart';
-
-/// {@template StarredReposScreen}
-/// A screen that displays a collection of starred repositories.
-/// {@endtemplate}
-class StarredReposScreen extends StatelessWidget {
-  /// Creates a starred repositories screen.
-  ///
-  /// {@macro StarredReposScreen}
+class StarredReposScreen extends ConsumerWidget {
   const StarredReposScreen();
 
   @override
-  Widget build(BuildContext context) => _Wrapper(
-        builder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Starred Repositorires'),
+  Widget build(BuildContext context, WidgetRef ref) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Starred Repositorires'),
+        ),
+        body: BlocConsumer<StarredReposCubit, StarredReposState>(
+          bloc: ref.watch(starredReposCubitPod),
+          listener: (context, starredReposState) => starredReposState.when(
+            loaded: (_, __, warning) => warning?.when(
+              offline: () {
+                ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                return ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'No reliable Internet connection. Showing cached data.',
+                    ),
+                  ),
+                );
+              },
             ),
-            body: BlocConsumer<StarredReposCubit, StarredReposState>(
-              listener: (context, starredReposState) => starredReposState.when(
-                loaded: (_, __, warning) => warning?.when(
-                  offline: () {
-                    ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                    return ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          'No reliable Internet connection. Showing cached data.',
+            loading: (_) {},
+          ),
+          builder: (context, starredReposState) {
+            final starredRepos = starredReposState.repos;
+            final starredReposCubit = ref.read(starredReposCubitPod);
+            return RefreshIndicator(
+              onRefresh: () => starredReposCubit.reload(),
+              child: starredRepos.isEmpty
+                  ? LayoutBuilder(
+                      builder: (context, constraints) => SingleChildScrollView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        child: SizedBox(
+                          height: constraints.maxHeight,
+                          child: Center(
+                            child: starredReposState.maybeWhen(
+                              loading: (_) => Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: const [
+                                  CircularProgressIndicator(),
+                                  SizedBox(height: 10),
+                                  Text('Loading'),
+                                ],
+                              ),
+                              orElse: () => const Text(
+                                "You haven't starred any repository yet",
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    );
-                  },
-                ),
-                loading: (_) {},
-              ),
-              builder: (context, starredReposState) {
-                final starredRepos = starredReposState.repos;
-                final starredReposCubit = context.read<StarredReposCubit>();
-                return RefreshIndicator(
-                  onRefresh: () => starredReposCubit.reload(),
-                  child: starredRepos.isEmpty
-                      ? LayoutBuilder(
-                          builder: (context, constraints) =>
-                              SingleChildScrollView(
-                            physics: const AlwaysScrollableScrollPhysics(),
-                            child: SizedBox(
-                              height: constraints.maxHeight,
-                              child: Center(
-                                child: starredReposState.maybeWhen(
-                                  loading: (_) => Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: const [
-                                      CircularProgressIndicator(),
-                                      SizedBox(height: 10),
-                                      Text('Loading'),
-                                    ],
-                                  ),
-                                  orElse: () => const Text(
-                                    "You haven't starred any repository yet",
-                                  ),
+                    )
+                  : ListView.builder(
+                      itemCount: starredRepos.length +
+                          starredReposState.maybeWhen(
+                            loading: (_) => 1,
+                            orElse: () => 0,
+                          ),
+                      itemBuilder: (context, index) {
+                        if (index == starredRepos.length) {
+                          return const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 5.0),
+                            child: LinearProgressIndicator(minHeight: 10.0),
+                          );
+                        }
+                        final starredRepo = starredRepos[index];
+                        if (starredRepo == starredRepos.last) {
+                          starredReposCubit.load();
+                        }
+                        return ListTile(
+                          key: ValueKey(starredRepo.urlPath),
+                          leading: AspectRatio(
+                            aspectRatio: 1,
+                            child: CachedNetworkImage(
+                              imageUrl: starredRepo.owner.avatarUrl,
+                              placeholder: (context, url) => const Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: CircularProgressIndicator(),
+                              ),
+                              errorWidget: (context, url, error) =>
+                                  const Center(
+                                child: Icon(
+                                  Icons.image,
                                 ),
                               ),
                             ),
                           ),
-                        )
-                      : ListView.builder(
-                          itemCount: starredRepos.length +
-                              starredReposState.maybeWhen(
-                                loading: (_) => 1,
-                                orElse: () => 0,
-                              ),
-                          itemBuilder: (context, index) {
-                            if (index == starredRepos.length) {
-                              return const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 5.0),
-                                child: LinearProgressIndicator(minHeight: 10.0),
-                              );
-                            }
-                            final starredRepo = starredRepos[index];
-                            if (starredRepo == starredRepos.last) {
-                              starredReposCubit.load();
-                            }
-                            return ListTile(
-                              key: ValueKey(starredRepo.urlPath),
-                              leading: AspectRatio(
-                                aspectRatio: 1,
-                                child: CachedNetworkImage(
-                                  imageUrl: starredRepo.owner.avatarUrl,
-                                  placeholder: (context, url) => const Padding(
-                                    padding: EdgeInsets.all(10.0),
-                                    child: CircularProgressIndicator(),
-                                  ),
-                                  errorWidget: (context, url, error) =>
-                                      const Center(
-                                    child: Icon(
-                                      Icons.image,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              title: Text(starredRepo.name),
-                              subtitle: Text(starredRepo.owner.username),
-                              trailing: AspectRatio(
-                                aspectRatio: 1,
-                                child: Column(
-                                  children: [
-                                    const Icon(Icons.star),
-                                    Text('${starredRepo.starsCount}'),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                );
-              },
-            ),
-          );
-        },
+                          title: Text(starredRepo.name),
+                          subtitle: Text(starredRepo.owner.username),
+                          trailing: AspectRatio(
+                            aspectRatio: 1,
+                            child: Column(
+                              children: [
+                                const Icon(Icons.star),
+                                Text('${starredRepo.starsCount}'),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            );
+          },
+        ),
       );
 }

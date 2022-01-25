0666994b58b1161999bfa3aff440cb611ceaf944
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-24 23:20:08 -0500

    feat: setup GQL client injection

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_my_starred_repos/features/auth/core/dependency_injection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:graphql/client.dart';
 
 import 'config.dart';
 import 'flavors.dart';
@@ -12,6 +14,23 @@ final flavorPod = Provider<Flavor>(
 final appConfigPod = Provider<AppConfig>(
   (_) => throw StateError(
     'The app config has not been initialized',
+  ),
+);
+
+final gqlClientPod = Provider<GraphQLClient>(
+  (ref) => GraphQLClient(
+    cache: GraphQLCache(),
+    link: Link.from([
+      AuthLink(
+        getToken: () async {
+          final credsStorage = ref.read(credsStoragePod);
+          final creds = await credsStorage.get();
+          final token = creds?.accessToken;
+          return token == null ? null : 'Bearer $token';
+        },
+      ),
+      HttpLink('https://api.github.com/graphql'),
+    ]),
   ),
 );
 

b1f94ed8a0bd3068587c58d7c38211f588c42f1f
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-24 23:22:37 -0500

    feat: setup profile-related instances injection

---
+++ lib/features/profile/core/dependency_injection.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_my_starred_repos/core/dependency_injection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:profile/profile.dart';
+
+final profileApiPod = Provider(
+  (ref) => ProfileApi(
+    gqlClient: ref.watch(gqlClientPod),
+  ),
+);
+
+final profileRepoPod = Provider(
+  (ref) => ProfileRepo(
+    profileApi: ref.watch(profileApiPod),
+  ),
+);
+
+final profileCubitPod = Provider.autoDispose((ref) {
+  final cubit = ProfileCubit(
+    profileRepo: ref.watch(profileRepoPod),
+  );
+  ref.onDispose(() => cubit.close());
+  return cubit;
+});

018be15b1afdf7e13be2ffcde2205abcf554125c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-24 23:17:36 -0500

    chore: expose profile-related elements

--- features/profile/lib/profile.dart
+++ features/profile/lib/profile.dart
@@ -1,1 +1,5 @@
 library profile;
+
+export 'package:profile/src/infrastructure/profile.api.dart';
+export 'package:profile/src/infrastructure/profile.repo.dart';
+export 'package:profile/src/state/profile.cubit.dart';

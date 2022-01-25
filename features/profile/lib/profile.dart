a4ec1e6b7919ddd61c59e17ef2a0e16c24854d4d
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-25 00:10:58 -0500

    feat: expose profile entity

--- features/profile/lib/profile.dart
+++ features/profile/lib/profile.dart
@@ -1,5 +1,6 @@
 library profile;
 
+export 'package:profile/src/domain/profile.entity.dart';
 export 'package:profile/src/infrastructure/profile.api.dart';
 export 'package:profile/src/infrastructure/profile.repo.dart';
 export 'package:profile/src/state/profile.cubit.dart';

5cd99c02207577689ba7d7b831c121fe7c9c8660
Karlo Verde <karlo.verde.1996@gmail.com>
2022-01-30 22:53:18 -0500

    test: ignore Freezed constructors for testing

--- features/starred_repos/lib/src/domain/get_starred_repos_warnings.dart
+++ features/starred_repos/lib/src/domain/get_starred_repos_warnings.dart
@@ -4,5 +4,7 @@ part 'get_starred_repos_warnings.freezed.dart';
 
 @freezed
 class GetStaredReposWarning with _$GetStaredReposWarning {
+  // coverage:ignore-start
   const factory GetStaredReposWarning.offline() = _GetStaredReposWarningOffline;
+  // coverage:ignore-end
 }
--- features/starred_repos/lib/src/domain/page.dart
+++ features/starred_repos/lib/src/domain/page.dart
@@ -4,10 +4,12 @@ part 'page.freezed.dart';
 
 @freezed
 class Page<T> with _$Page<T> {
+  // coverage:ignore-start
   const factory Page({
     required int lastPage,
     required List<T> elements,
   }) = _Page<T>;
+  // coverage:ignore-end
 }
 
 typedef Mapper<I, O> = O Function(I element);

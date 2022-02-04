4c24a67918a5f3f30d67f2ea26c21bb9ca6b2246
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-03 23:01:01 -0500

    chore: remove unrelated features

--- lib/features/users_placeholder/domain/get_users_failure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'get_users_failure.freezed.dart';
-
-@freezed
-class GetUsersFailure with _$GetUsersFailure {
-  const factory GetUsersFailure.offline() = _GetUsersFailureOffline;
-}

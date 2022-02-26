f5317ad76010a2075eb535a755d4593713c7aff4
Karlo Verde <karlo.verde.1996@gmail.com>
2022-02-25 22:53:07 -0500

    feat: persist total pages

--- features/starred_repos/lib/src/infrastructure/starred_repos.storage.dart
+++ features/starred_repos/lib/src/infrastructure/starred_repos.storage.dart
@@ -7,60 +7,86 @@ class StarredReposStorage {
   StarredReposStorage({
     required Database sembastDatabase,
   })  : _db = sembastDatabase,
-        store = StoreRef(storeName);
+        starredReposStore = StoreRef(starredReposStoreName),
+        totalPagesStore = StoreRef(totalPagesStoreName);
 
   final Database _db;
 
   @visibleForTesting
-  final StoreRef<int, Map<String, dynamic>> store;
+  final StoreRef<int, Map<String, dynamic>> starredReposStore;
 
   @visibleForTesting
-  static const storeName = 'starred_repos';
+  final StoreRef<String, int> totalPagesStore;
 
+  @visibleForTesting
+  late final RecordRef<String, int> totalPagesRecord =
+      totalPagesStore.record(totalPagesRecordKey);
+
+  @visibleForTesting
+  static const starredReposStoreName = 'starred_repos';
+
+  @visibleForTesting
+  static const totalPagesStoreName = 'total_pages';
+
+  @visibleForTesting
+  static const totalPagesRecordKey = 'total_pages';
+
+  /// Persists the [starredReposPage] to the database.
+  ///
+  /// The [pageNumber] is 1-based.
   Future<void> setPage({
     required int pageNumber,
-    required List<GithubRepo> starredRepos,
+    required Page<GithubRepo> starredReposPage,
   }) async {
-    final pageLength = starredRepos.length;
+    final starredRepos = starredReposPage.elements;
+    final pageLength = starredReposPage.elements.length;
+    final offset = (pageNumber - 1) * pageLength;
+    final keys = List.generate(
+      pageLength,
+      (idx) => offset + idx + 1, // Keys are 1-based.
+    );
     await _db.transaction<void>(
       (transaction) async {
-        await store.delete(
+        await starredReposStore.records(keys).put(
+              transaction,
+              starredRepos.map((r) => r.toJson()).toList(),
+            );
+        await totalPagesRecord.put(
           transaction,
-          finder: Finder(
-            offset: (pageNumber - 1) * pageLength,
-          ),
-        );
-        await store.addAll(
-          transaction,
-          starredRepos.map((r) => r.toJson()).toList(),
+          starredReposPage.lastPage,
         );
       },
     );
   }
 
-  Future<Page<GithubRepo>?> getPage({
+  /// Retrieves a page of starred repos from the database.
+  ///
+  /// The [pageNumber] is 1-based.
+  Future<Page<GithubRepo>> getPage({
     required int pageNumber,
     required int pageLength,
-  }) async =>
-      _db.transaction(
-        (transaction) async {
-          final reposCount = await store.count(transaction);
-          if (reposCount == 0) return null;
-          final lastPage = ((reposCount - 1) ~/ pageLength) + 1;
-          final foundReposSnapshots = await store.find(
-            transaction,
-            finder: Finder(
-              offset: (pageNumber - 1) * pageLength,
-              limit: pageLength,
-            ),
-          );
-          final foundRepos = foundReposSnapshots
-              .map((snapshot) => GithubRepo.fromJson(snapshot.value))
-              .toList();
-          return Page<GithubRepo>(
-            lastPage: lastPage,
-            elements: foundRepos,
-          );
-        },
-      );
+  }) async {
+    final offset = (pageNumber - 1) * pageLength;
+    final keys = List.generate(pageLength, (idx) => offset + idx);
+    return _db.transaction(
+      (transaction) async {
+        final rawFoundRepos =
+            await starredReposStore.records(keys).get(transaction);
+        final foundRepos = rawFoundRepos
+            .where((rawRepo) => rawRepo != null)
+            .map<GithubRepo>((rawRepo) => GithubRepo.fromJson(rawRepo!))
+            .toList();
+        final storedStarredReposLastPage = await totalPagesRecord.get(
+          transaction,
+        );
+        final reposCount = await starredReposStore.count(transaction);
+        final computedStarredReposLastPage =
+            ((reposCount - 1) ~/ pageLength) + 1;
+        return Page<GithubRepo>(
+          lastPage: storedStarredReposLastPage ?? computedStarredReposLastPage,
+          elements: foundRepos,
+        );
+      },
+    );
+  }
 }
